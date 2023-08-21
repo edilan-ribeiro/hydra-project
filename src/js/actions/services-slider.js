d873949edab0d361abf4fce5b646faf6ac92d124
edilan-ribeiro <eddyoneal2@hotmail.com>
2023-08-21 15:34:30 -0300

    services section mobile slider screated

---
+++ src/js/actions/services-slider.js
@@ -0,0 +1,51 @@
+let sliderContainer = document.getElementById('services-slider-container');
+let aboutSliderItems = document.querySelectorAll('.service-square');
+let aboutRightArrow = document.getElementById('svc-ar-right');
+let aboutLeftArrow = document.getElementById('svc-ar-left');
+
+let aboutIndex = 0;
+
+aboutRightArrow.addEventListener('click', function() {
+    aboutIndex++;
+    if (aboutIndex > aboutSliderItems.length - 1) {
+      aboutIndex = 0;
+    }
+   
+    slide();
+  });
+  
+  
+  aboutLeftArrow.addEventListener('click', function() {
+    aboutIndex--;
+    if (aboutIndex < 0) {
+      aboutIndex = aboutSliderItems.length - 1;
+    }
+    
+    slide();
+  });
+  
+
+  function slide() {
+    let itemWidth = aboutSliderItems[0].clientWidth;
+    let gap = 20;
+  
+    for (let i = 0; i < aboutSliderItems.length; i++) {
+      aboutSliderItems[i].style.transform = 'translateX(' + ((i - aboutIndex) * (itemWidth + gap)) + 'px)';
+    }
+  }
+  
+
+
+function applyTransform() {
+    if (window.innerWidth < 931) {
+      aboutSliderItems[0].style.transform = 'translateX(0px)';
+      aboutSliderItems[1].style.transform = 'translateX(330px)';
+      aboutSliderItems[2].style.transform = 'translateX(660px)';
+      aboutSliderItems[3].style.transform = 'translateX(990px)';
+    } else {
+      aboutSliderItems.forEach(item => item.style.transform = '');
+    }
+  }
+  
+
+  export {applyTransform}
